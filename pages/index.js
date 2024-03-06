05567792c0d24867e00ed17e320b88c3e74803b2
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 14:59:06 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,31 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import InputField from '../pages/components/InputField';
 
 function Home() {
   const newRef = useRef();
   const [values, setValues] = useState('');
-
+  const [input,setInput] = useState('');
   const focusInput = () => {
-
+    newRef.current.focus();
   };
   const settingValue = () => {
-
+    setValues("");
+    setValues(input);
   };
+  useEffect(()=>{
+    settingValue();
+  },[values]);
 
+  const inputHolder =(e)=>{setInput(e.target.value)}
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/>
+    <InputField  ref={newRef}  type="text" inputChange={inputHolder}/>
     <br/>
     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
+
     <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
      <textarea id="textarea" value={values}></textarea>
-
+     {/*  */}
     </div>
   );
 }
