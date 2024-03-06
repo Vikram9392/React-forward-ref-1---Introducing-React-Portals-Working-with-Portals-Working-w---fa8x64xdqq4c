a3393f8126b277662069854b7d6f765f5ce8412b
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 14:58:19 +0530

    Update InputField.js

--- pages/components/InputField.js
+++ pages/components/InputField.js
@@ -2,9 +2,10 @@
 import React from 'react';
 
 const InputField = React.forwardRef((props, ref) => {
+  console.log("nhvtf : ",props.inputChange);
   return (
     <div>
-      <input id='input' type='text' ref={ref} />
+      <input id='input' type='text' ref={ref} onChange={props.inputChange}/>
     </div>
   );
 });
